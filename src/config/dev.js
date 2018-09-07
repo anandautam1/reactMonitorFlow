926dd9f4815488382590eb3aec9efe3fb60054d7
Ananda Utama <ananda_utama@y7mail.com>
2018-09-07 23:38:28 +1000

    Delete dev.js

--- src/config/dev.js
+++
@@ -1,8 +0,0 @@
-export const FirebaseConfig = {
-    apiKey: "AIzaSyCChGgKey1oFWjrVCQXZ2Qio3ObS59PIWQ",
-    authDomain: "simpleexample-14b55.firebaseapp.com",
-    databaseURL: "https://simpleexample-14b55.firebaseio.com",
-    projectId: "simpleexample-14b55",
-    storageBucket: "simpleexample-14b55.appspot.com",
-    messagingSenderId: "232129477518"
-};
